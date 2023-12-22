40ffc9f917f5b0a483d6f4e6aef535a6d0a5e85e
jamiemharper98 <jamiemharper98@gmail.com>
2023-12-22 17:47:37 +0000

    seeding

--- seed.js
+++ seed.js
@@ -5,6 +5,7 @@ const allUsers = require("./data/userdata");
 const MongoClient = require("mongodb").MongoClient;
 
 async function seedDB() {
+  /// replace uri with link to atlas db - not sure if it works with local mongo, mine hasnt installed yet
   const uri = "mongodb+srv://admin:Password@cluster0.35psafi.mongodb.net/Node-API?retryWrites=true&w=majority";
   const client = new MongoClient(uri);
   try {
@@ -20,23 +21,28 @@ async function seedDB() {
     mngdb.collection("Categories").insertMany(allCategories);
     const catsForSeed = await mngdb.collection("Categories").find({}).toArray();
     const usersForSeed = await mngdb.collection("Users").find({}).toArray();
+    const formattedQuotes = formatQuotes(allQuotes, usersForSeed, catsForSeed);
 
-    for (const quote of allQuotes) {
-      for (const user of usersForSeed) {
-        if (user.username === quote.quoteUser) {
-          quote.userId = user._id;
-          delete quote.quoteUser;
-        }
-      }
-      for (const cat of catsForSeed) {
-        if (cat.categoryName === quote.quoteCategory) {
-          quote.categoryId = cat._id;
-          delete quote.quoteCategory;
-        }
-      }
-    }
-    mngdb.collection("Quotes").insertMany(allQuotes);
+    mngdb.collection("Quotes").insertMany(formattedQuotes);
   } catch {}
 }
 
 seedDB();
+
+const formatQuotes = (allQuotes, usersForSeed, catsForSeed) => {
+  for (const quote of allQuotes) {
+    for (const user of usersForSeed) {
+      if (user.username === quote.quoteUser) {
+        quote.userId = user._id;
+        delete quote.quoteUser;
+      }
+    }
+    for (const cat of catsForSeed) {
+      if (cat.categoryName === quote.quoteCategory) {
+        quote.categoryId = cat._id;
+        delete quote.quoteCategory;
+      }
+    }
+  }
+  return allQuotes;
+};
